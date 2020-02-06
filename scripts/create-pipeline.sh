92c78c3493644f020b7fc3ad62e0ec67adfa7fb8
Agrianitis, Eleftherios <eleftherios.agrianitis@kpn.com>
2020-02-06 19:45:36 +0100

    test

---
+++ scripts/create-pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -n "Github OAuth Token: <hidden> "
+read -r -s token
+if [ ${#token} != 40 ]
+then
+  echo -e "\nYour Github Access Key is not correct. It needs to be 40 alpnumerical charachers"
+  exit 1
+fi
+
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+set -eu
+
+aws cloudformation create-stack \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --stack-name "$CODEPIPELINE_STACK_NAME" \
+        --parameters ParameterKey=GitHubOAuthToken,ParameterValue="${token}" \
+                     ParameterKey=GitHubOwner,ParameterValue="${1}" \
+                     ParameterKey=GitHubRepo,ParameterValue="${2}" \
+                     ParameterKey=NotificationEmailAddress,ParameterValue="${3}" \
+        --template-body file://../templates/pipeline.template.yaml \
+        --profile "$AWS_PROFILE"
+
+echo "Cloudformation create stack operation sucessfully completed"
