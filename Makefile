eab5f3403cabd0d090975ce548c7cb320d921615
Lefteris Agrianitis <agria500@Lefteriss-MacBook-Pro.local>
2020-02-07 08:55:28 +0100

    test

--- Makefile
+++ Makefile
@@ -1,4 +1,28 @@
-.PHONY all create-pipeline
+SHELL:=/bin/bash
+include makefile.env
+AWS_PROFILE?=default
 
-create-pipile:
- make -C templates/ create-pipeline
+.PHONY: all help login create-pipeline update-pipeline delete-pipeline clean-all
+
+help: ## All possible make arguements targets
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | xsort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+## Implicit guard target, that checks that the variable in the stem is defined.
+guard-%:
+	@if [ -z '${${*}}' ]; then echo "Environment variable $* not set"; exit 1; fi
+
+check-env: guard-AWS_PROFILE guard-CODEPIPELINE_STACK_NAME guard-GITHUB_OWNER guard-GITHUB_REPO guard-SNS_EMAIL_ADDRESS
+
+check-repo:
+	@git ls-remote https://github.com/$(GITHUB_OWNER)/$(GITHUB_REPO) > /dev/null \
+	&& exit 0 \
+	|| echo "Github owner or repo doesn't exit"; exit 1
+
+create-pipeline: check-env check-repo
+	@/bin/bash scripts/create-pipeline.sh
+update-pipeline:
+	@/bin/bash scripts/update-pipeline.sh
+delete-pipeline:
+	@/bin/bash scripts/delete-pipeline.sh
+
+clean-all: create-pipeline update-pipeline delete-pipeline
--- scripts/update-pipeline.sh
+++ scripts/update-pipeline.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo -n "Github OAuth Token: <hidden> "
 read -r -s token
 if [ ${#token} != 40 ]
@@ -12,14 +11,14 @@ fi
 # -u: Treat unset variables as an error when substituting.
 set -eu
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
         --capabilities CAPABILITY_NAMED_IAM \
         --stack-name "$CODEPIPELINE_STACK_NAME" \
         --parameters ParameterKey=GitHubOAuthToken,ParameterValue="${token}" \
-                     ParameterKey=GitHubOwner,ParameterValue="${GITHUB_OWNER}" \
-                     ParameterKey=GitHubRepo,ParameterValue="${GITHUB_REPO}" \
-                     ParameterKey=NotificationEmailAddress,ParameterValue="${SNS_EMAIL_ADDRESS}" \
-        --template-body file://./templates/pipeline.template.yaml \
+                     ParameterKey=GitHubOwner,ParameterValue="${1}" \
+                     ParameterKey=GitHubRepo,ParameterValue="${2}" \
+                     ParameterKey=NotificationEmailAddress,ParameterValue="${3}" \
+        --template-body file://../templates/pipeline.template.yaml \
         --profile "$AWS_PROFILE"
 
 echo "Cloudformation create stack operation sucessfully completed"
